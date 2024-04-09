afef4b4cdf3f3d159b6f7eca5541f9363eecc93e
Mansur <1.mansura.2005@gmail.com>
2024-04-09 16:52:54 +0100

    Showing the data visualization

--- Python Stock AI Project.py
+++ Python Stock AI Project.py
@@ -1,5 +1,7 @@
 #Version 1
-
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import yfinance as yf 
 #calls api to fetcxh data
 
@@ -10,7 +12,7 @@ sp500 = sp500.history(period="max")
 #cleaning and visualizing stock market data
 
 sp500.plot.line(y="Close", use_index=True)
-
+plt.show()
 
 
 
