6abb50827d29b4321bad1fa9bc9f095f93805887
Mansur <1.mansura.2005@gmail.com>
2024-04-09 16:34:42 +0100

    Setting up inital data

--- Python Stock AI Project.py
+++ Python Stock AI Project.py
@@ -1,6 +1,15 @@
 #Version 1
 
-import yfinance
+import yfinance as yf 
+#calls api to fetcxh data
+
+#calling sp500 stock data
+sp500 = yf.Ticker("^GSPC")
+sp500 = sp500.history(period="max")
+
+#cleaning and visualizing stock market data
+
+sp500.plot.line(y="Close", use_index=True)
 
 
 
